e16e36655c558da03e0f433cb30840e32222a9f0
baljor3 <baljor20@gmail.com>
2024-01-15 14:25:09 -0800

    fixing deployment

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter base={"https://baljor3.github.io/PersonalWebsite"}>
   <App />
-  </BrowserRouter>
-    
-
-    
 
 );
 
