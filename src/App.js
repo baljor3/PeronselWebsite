4d5b67948115b4105a3d40e2b0908138dacb9ec0
baljor3 <baljor20@gmail.com>
2024-01-15 23:18:36 -0800

    homepage value change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './sectionfiles/nav';
+
 import Home from './sectionfiles/Homepage';
 
 const App = () => {
