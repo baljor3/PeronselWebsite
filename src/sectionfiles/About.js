8645af88bb8acf2e99a4b95b8638dcf49f7a18eb
Baljor Dhillon <baljor20@gmail.com>
2024-03-24 22:06:38 -0700

    fix bug

--- src/sectionfiles/About.js
+++ src/sectionfiles/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = React.forwardRef((props,ref)=>{
 
     return(<div ref = {ref}id="about" className="text-center pt-10 pb-10 bg-gray-600">
-        Baljor Dhillon <br/>
+        Bobby Dhillon <br/>
         I am a full stack developer from Vancouver,B.C., Canada.<br></br>
         I am passionate about learning more and gaining new skills.<br></br>
         My projects are listed below.
